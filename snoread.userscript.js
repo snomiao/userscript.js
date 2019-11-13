6bc14bb240d033384b8d884410e30469cce2d5d8
snomiao <snomiao@gmail.com>
2019-11-13 12:01:03 +0800

    style 迁移

--- snoread.userscript.js
+++ snoread.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         雪阅模式|SNOREAD
 // @namespace    https://userscript.snomiao.com/
-// @version      0.5(20191111)
+// @version      0.6(20191111)
 // @description  【自用，目前还有很多BUG】豪华广角宽屏视角 / 横向滚动阅读模式 / 翻页模式 / 充分利用屏幕空间
 // @author       snomiao@gmail.com
 // @match        http*://*/*
@@ -10,13 +10,65 @@
 
 (function () {
     'use strict';
+
+    var updateStyle = () => {
+        var eStyle = document.querySelector("div.snomiao-article-style")
+        if(!eStyle){
+            eStyle = document.createElement("div");
+            eStyle.classList.add("snomiao-article-style");
+            eStyle.style.display = "none";
+            document.body.appendChild(eStyle)
+        }
+        var windowHeight = window.innerHeight;
+        var windowWidth = window.innerWidth;
+        eStyle.innerHTML = `
+            <style>
+            .snomiao-article{
+                position: relative;
+                top: 0;
+                max-height: ${windowHeight * 0.9}px;
+                box-sizing: border-box;
+                width: calc(${windowWidth}px - 2em);
+            
+                display: flex;
+                flex-flow: column;
+                flex-wrap: wrap;
+                align-content: flex-start;
+            
+                overflow-x: auto;
+                overflow-y: hidden;
+            
+                z-index:1;
+                
+                box-shadow: 0 0 1px blue;
+                background: rgba(255,255,255,0.5);
+                color: black;
+            
+                text-align: justify;
+                text-indent: 0;
+            }
+            .snomiao-article>*{
+                margin: 0 -1em 0 0;
+                padding: 0 2em 1em 0;
+                /* max-width: 70em; */
+                min-width: 30em;
+                width: min-content;
+                max-height: ${windowHeight * 0.9}px;
+                height:auto;
+                overflow-x: auto;
+                overflow-y: auto;
+                background: rgba(255,255,255,0.5);
+            }
+            </style>
+        `;
+    }
+
     var listenScroll = e => {
         [...e.children].map(listenScroll)
         if (e.flag_listenScroll) return;
         e.flag_listenScroll = 1;
 
         var handleScroll = (event) => {
-            
             var scrollRate = (event.detail || -event.wheelDelta) / 120 * 0.5
             var scrolled_x = (e.scrollLeft != (e.scrollLeft += scrollRate * e.clientWidth, e.scrollLeft))
             if (scrolled_x) {
@@ -26,7 +78,6 @@
                 event.stopPropagation();
                 return false;
             }
-            
             var scrolled_y = (e.scrollTop != (e.scrollTop += scrollRate * e.clientHeight, e.scrollTop))
             if (scrolled_y) {
                 e.scrollIntoViewIfNeeded()
@@ -40,10 +91,9 @@
         e.addEventListener("mousewheel", handleScroll, { capture: false, passive: false })     // Chrome/Edge
         e.addEventListener("DOMMouseScroll", handleScroll, { capture: false, passive: false }) // FF
     }
-    //listenScroll(document.documentElement)
     var turnBackNormal = e => {
         e.setAttribute("style", ``);
-        [...e.children].map(e => e.setAttribute("style", ``))
+        // [...e.children].map(e => e.setAttribute("style", ``))
         e.classList.remove("snomiao-article")
     }
     var listenClick = e => {
@@ -66,68 +116,16 @@
         }, true);
         e.flag_handleClickToggleSnoReadMode = 1
     }
-    
+
     var processArticle = (e) => {
         turnBackNormal(e)
-        var wHeight = window.innerHeight;
-        var screenWidth = window.innerWidth;
-        window.snomiao_article=e
+        window.snomiao_article = e
         listenScroll(e)
         listenClick(e)
-
-        // console.log("setStyle", e, level)
-        // e.setAttribute("style", `position: relative;left: 0;top: 0;`)
         var { left, top } = e.getBoundingClientRect()
         e.classList.add("snomiao-article")
-
-        e.setAttribute("style", `
-        position: relative;
-        left: calc(1em + ${-left}px);
-        top: 0;
-        max-height: ${wHeight * 0.9}px;
-        box-sizing: border-box;
-        width: calc(${screenWidth}px - 2em);
-
-        display: flex;
-        flex-flow: column;
-        flex-wrap: wrap;
-        align-content: flex-start;
-
-        overflow-x: auto;
-        overflow-y: hidden;
-
-        z-index:1;
-        
-        box-shadow: 0 0 1px blue;
-        background: rgba(255,255,255,0.5);
-        color: black;
-
-        text-align: justify;
-        text-indent: 0;
-    `);
-        [...e.children].map(c => {
-            c.setAttribute("style", `
-                margin: 0 -1em 0 0;
-                padding: 0 2em 1em 0;
-                max-width: 70em;
-                min-width: 30em;
-                width: min-content;
-                max-height: ${wHeight * 0.9}px;
-                height:auto;
-                overflow-x: auto;
-                overflow-y: auto;
-                background: rgba(255,255,255,0.5);
-
-            `)
-        })
-        // var e2 = document.create(e.tagName)
-        // e.parentElement.appendChild()
-        // e.innerHTML
-        // e.scrollTo()
-
-        // e.scrollIntoView(true);
-        // window.scrollBy(0, wHeight * -0.05)
-        // window.a = e
+        e.setAttribute("style", `left: calc(1em + ${-left}px)`);
+        updateStyle()
     }
     // processArticle(window.article)
 
@@ -142,12 +140,11 @@
         var sub = large2.map(e => getArticles(e, level + 1))
         var isArticle = !large2.length
         var rate = e.offsetHeight / e.parentElement.offsetHeight
-        
         return { e, isArticle, rate, sub }
     }
     var setArticle = ({ e, isArticle, sub }) => {
         sub.map(setArticle);
-        if(e == document.body) return;
+        if (e == document.body) return;
         isArticle && console.log(e, "isArticle");
         isArticle && processArticle(e);
     }
@@ -159,7 +156,7 @@
         setArticle(articleTree)
     }
     // setInterval(main, 3000)
-    // document.addEventListener("onload", main)
+    document.addEventListener("load", main)
     window.addEventListener("load", main)
     window.addEventListener("resize", main)
     main()
