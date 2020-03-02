de38b4795242483d2efaf3b0f3b92554545548f6
snomiao <snomiao@gmail.com>
2020-03-02 13:35:25 +0800

    urlsearchparams

--- 超星提前解锁章节.userscript.js
+++ 超星提前解锁章节.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         超星提前解锁章节
 // @namespace    https://userscript.snomiao.com
-// @version      0.1
+// @version      0.3
 // @description  我是热爱学习的好孩子
 // @author       snomiao@gmail.com
 // @match        https://*.chaoxing.com/mycourse/studentstudy?*
@@ -9,20 +9,20 @@
 
 (() => {
     var main = () => {
-        var query = window.location.search;
-        var courseId = query.match(/(?<=courseId=)\d+/)[0];
-        var clazzid = query.match(/(?<=clazzid=)\d+/)[0];
-        [...document.querySelectorAll("h4>a")].map(a => {
-            var h4 = a.parentElement;
-            var chapterId = h4.id.match(/(?<=cur)\d+/)[0];
-            /* 表示已解锁 */
-            var rp = h4.querySelector('.roundpointStudent.lock');
-            rp && (rp.innerHTML = '/');
-            /* 激活链接 */
-            a.href = `javascript: getTeacherAjax('` + courseId + `','` + clazzid + `','` + chapterId + `');`;
-        })
-    }
-    setInterval(main, 10000)
+            var query = new URLSearchParams(window.location.search);
+            var courseId = query.get('courseId');
+            var clazzid = query.get('clazzid');
+            [...document.querySelectorAll("h4>a")].map(a => {
+                var h4 = a.parentElement;
+                var chapterId = h4.id.match(/(?<=cur)\d+/)[0];
+                /* 表示已解锁 */
+                var rp = h4.querySelector('.roundpointStudent.lock');
+                rp && (rp.innerHTML = '/');
+                /* 激活链接 */
+                a.href = `javascript: getTeacherAjax('` + courseId + `','` + clazzid + `','` + chapterId + `');`;
+            })
+        }
+        // setInterval(main, 10000)
     window.addEventListener("onload", main)
     document.addEventListener("onload", main)
     main()
