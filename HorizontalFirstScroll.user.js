1622b2b8f4188afd8f126e5b178702dc763af222
snomiao <snomiao@gmail.com>
2020-04-09 08:03:31 +0800

    分离滚动

---
+++ HorizontalFirstScroll.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Horizontal First Scroll | 横向优先滚动
+// @namespace    https://userscript.snomiao.com/
+// @version      1.0
+// @description  (20200409)Page elements prefer to scroll horizontally, scroll to end and then scroll vertically, suitable for SNOREAD.userscript.js | 页面元素优先横向滚动，滚动到底再进行竖向滚动，适用于雪阅插件
+// @author       snomiao@gmail.com
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+// 横向滚动
+(function () {
+    'use strict';
+    var 监听滚动 = e => {
+        [...e.children].map(监听滚动)
+        if (e.flag_已监听滚动) return;
+        e.flag_已监听滚动 = 1;
+
+        var handleScroll = (事件) => {
+            if (事件.altKey || 事件.ctrlKey || 事件.shiftKey) return;
+            var scrollRate = (事件.detail || -事件.wheelDelta) / 120 //Y轴
+            var scrolled_x = (e.scrollLeft != (e.scrollLeft += scrollRate * e.clientWidth * 0.1, e.scrollLeft))
+            if (scrolled_x) {
+                // 若需定位则撤销滚动
+                var 当前Y = e.getBoundingClientRect().y
+                e.scrollIntoViewIfNeeded()
+                if (e.getBoundingClientRect().y != 当前Y)
+                    e.scrollLeft -= scrollRate * e.clientWidth * 0.1;
+                //
+                事件.preventDefault();
+                事件.stopPropagation();
+                return false;
+            }
+            var scrolled_y = (e.scrollTop != (e.scrollTop += scrollRate * e.clientHeight * 0.5, e.scrollTop))
+            if (scrolled_y) {
+                var 当前X = e.getBoundingClientRect().x
+                e.scrollIntoViewIfNeeded()
+                if (e.getBoundingClientRect().x != 当前X)
+                    e.scrollTop -= scrollRate * e.clientHeight * 0.5;
+                //
+                事件.preventDefault();
+                事件.stopPropagation();
+                return false;
+            }
+            // 横竖都滚到底了
+            [...e.children].map(监听滚动)
+        }
+        // e.addEventListener("mousewheel", handleScroll, { capture: false, passive: false }) // Chrome/Edge
+        e.addEventListener("DOMMouseScroll", handleScroll, { capture: false, passive: false }) // FF
+    }
+    var 入口 = () => 监听滚动(document.body)
+    document.addEventListener("DOMContentLoaded", 入口)
+    window.addEventListener("load", 入口)
+    入口()
+})();
--- snoread.userscript.js
+++ snoread.userscript.js
@@ -19,10 +19,21 @@
 // 尝试清除挡物
 // javascript:[...document.querySelectorAll("*")].filter(e=>e).filter(e=>window.getComputedStyle(e).getPropertyValue("z-index")>1).
 
-(function() {
+(function () {
     'use strict';
     'esversion: 6';
+
     var 睡 = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    var 异步防抖函数 = async (异步函数, 间隔时间 = 1000) => {
+        var 上次执行时间 = null
+        return async (...参列) => {
+            if (上次执行时间 && 上次执行时间 + 间隔时间 <= +new Date())
+                return null
+            上次执行时间 = +new Date()
+            return await 异步函数(参列)
+        }
+    }
+    
     // var 窗口高 = window.innerHeight; // Math.min(window.innerHeight, window.outerHeight);
     // var 窗口高 = // window.innerHeight; // Math.min(window.innerHeight, window.outerHeight);
     // document.body.clientWidth; //window.innerWidth; // Math.min(window.innerWidth, window.outerWidth);
@@ -105,20 +116,20 @@ div#main-wrapper:after, .clearfix:after {
     var 监听点击 = 元素 => {
         if (元素.flag_handleClickToggleSnoReadMode) return;
         // click to update this article and scroll to it
-        元素.addEventListener("click", function(事件) {
+        元素.addEventListener("click", function (事件) {
             // console.debug("点击元素", 元素)
             元素.scrollIntoViewIfNeeded()
             元素.classList.contains("snomiao-article") &&
                 进入雪阅模式(元素)
         }, false);
         // dblclick to turn back
-        元素.addEventListener("dblclick", function(事件) {
+        元素.addEventListener("dblclick", function (事件) {
             if (!事件.altKey) return;
             // console.log(元素, 元素.classList.contains("snomiao-article"))
             元素.classList.contains("snomiao-article") &&
                 (退出雪阅模式(元素) || true) ||
                 进入雪阅模式(元素)
-                // 元素.scrollLeft += 元素.clientWidth + 100
+            // 元素.scrollLeft += 元素.clientWidth + 100
             事件.preventDefault();
         }, true);
         元素.flag_handleClickToggleSnoReadMode = 1
@@ -143,12 +154,12 @@ div#main-wrapper:after, .clearfix:after {
         }
     }
     var 适配元素位置到屏幕 = (元素) => {
-            元素.setAttribute("style", `left: 0`);
-            var { left, top } = 元素.getBoundingClientRect()
-            元素.classList.add("snomiao-article")
-            元素.setAttribute("style", `left: calc(${-left}px)`);
-        }
-        // 适配元素位置到屏幕(temp1)
+        元素.setAttribute("style", `left: 0`);
+        var { left, top } = 元素.getBoundingClientRect()
+        元素.classList.add("snomiao-article")
+        元素.setAttribute("style", `left: calc(${-left}px)`);
+    }
+    // 适配元素位置到屏幕(temp1)
 
     var 进入雪阅模式 = (元素) => {
         退出雪阅模式(元素)
@@ -157,8 +168,8 @@ div#main-wrapper:after, .clearfix:after {
         修复元素可见性(元素)
         更新样式()
         适配元素位置到屏幕(元素)
-            // ref: 适配此页面https://medium.com/s/story/why-sleep-on-it-is-the-most-useful-advice-for-learning-and-also-the-most-neglected-86b20249f06d
-            // 未知原因错位，不过写2次就能正常了
+        // ref: 适配此页面https://medium.com/s/story/why-sleep-on-it-is-the-most-useful-advice-for-learning-and-also-the-most-neglected-86b20249f06d
+        // 未知原因错位，不过写2次就能正常了
         适配元素位置到屏幕(元素)
         元素.flag_雪阅模式 = true
         console.debug(元素, "进入雪阅模式");
@@ -201,11 +212,9 @@ div#main-wrapper:after, .clearfix:after {
         var 窗口高 = 取窗口高(),
             窗口宽 = 取窗口宽()
         var 元素列 = 文章树取元素(文章树).flat(Infinity)
-        var 文章列 = 元素列.filter(e => e.flag_是文章)
-        var 文章列 = 排序按(取元素投影顶)(文章列)
-            // console.log(文章列)
-        var 相邻对列 = 取相邻对(文章列)
-        var 相邻对列 = 相邻对列.map(对 => (对.距离 = 取距离按(取元素投影顶)(...对), 对))
+        var 文章列 = 排序按(取元素投影顶)(元素列.filter(e => e.flag_是文章))
+        // console.log(文章列)
+        var 相邻对列 = 取相邻对(文章列).map(对 => (对.距离 = 取距离按(取元素投影顶)(...对), 对))
         var 异常对列 = 相邻对列.filter(对 => 对.距离 < 窗口高)
         var 冲突弱势元素表 = 异常对列.map(异常对 => 排序按(取元素面积)(异常对)[0])
         冲突弱势元素表.forEach(元素 => 元素.flag_冲突弱势元素 = true)
@@ -223,14 +232,14 @@ div#main-wrapper:after, .clearfix:after {
 
         // debug start
         子元素.forEach(e => {
-                e.高度占比 = 取元素投影高(e) / 元素外高
-                元素.setAttribute("高度占比", e.高度占比)
-                元素.setAttribute("高于窗口", 取元素投影高(e) > 窗口高)
-            })
-            // var 子元素叠高 = 子元素.map(e => e.offsetHeight).concat([0]).reduce((a, b) => a + b)
+            e.高度占比 = 取元素投影高(e) / 元素外高
+            元素.setAttribute("高度占比", e.高度占比)
+            元素.setAttribute("高于窗口", 取元素投影高(e) > 窗口高)
+        })
+        // var 子元素叠高 = 子元素.map(e => e.offsetHeight).concat([0]).reduce((a, b) => a + b)
         元素.setAttribute("是文章", 是文章)
         元素.setAttribute("占比", 占比)
-            // debug end
+        // debug end
 
 
         var 子树列 = 子元素高于屏.map(e => 取文章树(e, 层数 + 1)) || []
@@ -255,67 +264,26 @@ div#main-wrapper:after, .clearfix:after {
             元素.flag_雪阅模式 = true
         }
         更新雪阅模式(元素)
-            // 进入雪阅模式(元素)
+        // 进入雪阅模式(元素)
     }
-    var 入口 = async() => {
-            console.log("LAUNCH：SNOREAD");
-            await 恢复所有文章样式_临时()
-                // await 睡(100)
-            var 文章树 = 取文章树(document.body)
-            window.调试文章树1 = 文章树
-            window.调试文章树2 = 输出文章树(文章树)
-                //console.log(输出文章树(文章树))
-            检测重叠冲突(文章树)
-            转换文章树(文章树)
-        }
-        // setInterval(入口, 3000)
-    document.addEventListener("load", 入口)
-    window.addEventListener("load", 入口)
+    var 扫描并转换文章树 = async () => {
+        console.log("LAUNCH：SNOREAD");
+        await 恢复所有文章样式_临时()
+        // await 睡(100)
+        var 文章树 = 取文章树(document.body)
+        window.调试文章树1 = 文章树
+        window.调试文章树2 = 输出文章树(文章树)
+        //console.log(输出文章树(文章树))
+        检测重叠冲突(文章树)
+        转换文章树(文章树)
+    }
+    var 入口 = 异步防抖函数(扫描并转换文章树)
+
+    window.addEventListener('load', 入口, false)
     window.addEventListener("resize", 入口)
+    window.addEventListener('keyup', 入口, false)
+    window.addEventListener('mouseup', () => setTimeout(入口, 100), true)
     window.addEventListener("keydown", e => e.code == "Escape" && 恢复所有文章样式())
     入口()
 })();
 
-
-// 横向滚动
-(function() {
-    'use strict';
-    var 监听滚动 = e => {
-        [...e.children].map(监听滚动)
-        if (e.flag_已监听滚动) return;
-        e.flag_已监听滚动 = 1;
-
-        var handleScroll = (事件) => {
-            if (事件.altKey || 事件.ctrlKey || 事件.shiftKey) return;
-            var scrollRate = (事件.detail || -事件.wheelDelta) / 120 //Y轴
-            var scrolled_x = (e.scrollLeft != (e.scrollLeft += scrollRate * e.clientWidth * 0.1, e.scrollLeft))
-            if (scrolled_x) {
-                // 若需定位则撤销滚动
-                var 当前Y = e.getBoundingClientRect().y
-                e.scrollIntoViewIfNeeded()
-                if(e.getBoundingClientRect().y != 当前Y)
-                    e.scrollLeft -= scrollRate * e.clientWidth * 0.1;
-                //
-                事件.preventDefault();
-                事件.stopPropagation();
-                return false;
-            }
-            var scrolled_y = (e.scrollTop != (e.scrollTop += scrollRate * e.clientHeight * 0.5, e.scrollTop))
-            if (scrolled_y) {
-                e.scrollIntoViewIfNeeded()
-                //
-                事件.preventDefault();
-                事件.stopPropagation();
-                return false;
-            }
-            // 横竖都滚到底了
-            [...e.children].map(监听滚动)
-        }
-        e.addEventListener("mousewheel", handleScroll, { capture: false, passive: false }) // Chrome/Edge
-        e.addEventListener("DOMMouseScroll", handleScroll, { capture: false, passive: false }) // FF
-    }
-    var 入口 = () => 监听滚动(document.body)
-    document.addEventListener("DOMContentLoaded", 入口)
-    window.addEventListener("load", 入口)
-    入口()
-})();
