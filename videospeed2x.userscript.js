1be19bc72dd7ab13ff6bf3eb4301f6c65572739c
snomiao <snomiao@gmail.com>
2020-04-23 04:53:30 +0800

    更新其它语言说明

--- videospeed2x.userscript.js
+++ videospeed2x.userscript.js
@@ -1,14 +1,19 @@
 // ==UserScript==
-// @name          所有视频2倍速度播放 / video speed 2x
-// @namespace     https://userscript.snomiao.com/
-// @description   这个世界的一切都太慢了啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
-// @author        snomiao@gmail.com
-// @match         *://*.youtube.com/*
-// @match         *://*/*
-// @exclude       *://mooc1-2.chaoxing.com/*
-// @version       0.1.1
+// @name             所有视频2倍速度播放 / video speed 2x
+// @name:en          Videos default at 2x speed
+// @name:ja          2倍速でのデフォルトのビデオ再生
+// @namespace        https://userscript.snomiao.com/
+// @description      这个世界的一切都太慢了啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+// @description:en   Everything in this world is too sloooooooooooooooooooooooooow
+// @description:ja   この世界のすべてが遅すぎるあああああああああああああああああああああああああああああああああああああああ
+// @author           snomiao@gmail.com
+// @match            *://*.youtube.com/*
+// @match            *://*/*
+// @exclude          *://mooc1-2.chaoxing.com/*
+// @run-at           document-end
+// @version          0.1.2
 // ==/UserScript==
-// (20200422)更新：规避超星
+// (20200422)Update: avoid chaoxing online courses (needs 1x speed to complete the task)
 
 (() => {
     var rate = 2
