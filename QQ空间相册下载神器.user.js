5098ada98960f5b367cab37030180f6346e07b56
snomiao <snomiao@gmail.com>
2020-05-18 13:04:59 +0800

    修bug

--- QQ空间相册下载神器.user.js
+++ QQ空间相册下载神器.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QQ空间相册下载神器
 // @namespace    https://userscript.snomiao.com/
-// @version      0.2(20200501)
+// @version      0.3
 // @description  批量下载小姐姐空间高清大图照片佳选插件～自动命名快速汲取dalao灵感～
 // @author       snomiao@gmail.com
 // @match        *://user.qzone.qq.com/*
@@ -10,6 +10,7 @@
 // @grant        none
 // ==/UserScript==
 // 更新：(20200501)改进下载逻辑，不再弹出窗口
+// 更新：(20200518)还是弹吧……
 //
 ; (() => {
     var 下载 = (url, filename = '') => {
@@ -25,36 +26,34 @@
         var m = decodeURI(location.hash).match(/(?<=#下载-).*/)
         if (!m) return null;
         var filename = m[0]
-        下载(location.href, filename)
-        parent.postMessage("下载完成-" + location.href, 'https://user.qzone.qq.com')
+        下载(location.href, filename);
+        (opener || parent).postMessage("下载完成-" + location.href, 'https://user.qzone.qq.com')
         window.close();
     })
-    var 新元素 = (innerHTML, attributes = {}) => {
-        var e = document.createElement("div");
-        e.innerHTML = innerHTML;
-        return Object.assign(e.children[0], attributes)
-    }
     var 智能下载 = (url, filename = '') => {
         var 当页域名 = location.hostname
         var 同源 = !!url.match(":/" + 当页域名 + "/")
         if (同源) return 下载(url, filename)
-        // 否则使用iframe
-        window.addEventListener("message", (e) => {
-            var m = e.data && e.data.match(/(?<=下载完成-)(.*)/)
-            if (!m) return;
-            let url = m[0];
-            // console.debug("删除iframe",e.origin, url);
-            // e.origin ignore
-            [...document.querySelectorAll("iframe")]
-                .filter(e => e.src == url)
-                .forEach(e => {
-                    // console.debug("删除了iframe",e.src, e);
-                    e.remove()
-                });
-        }, false);
-        document.body.appendChild(新元素(
-            '<iframe style="visibility: hidden"></iframe>',
-            { src: url + '#下载-' + encodeURI(filename) }))
+        // 否则使用 iframe // iframe会漏照片……闲了再调试好了
+        // window.addEventListener("message", (e) => {
+        //     var m = e.data && e.data.match(/(?<=下载完成-)(.*)/)
+        //     if (!m) return;
+        //     let url = m[0];
+        //     // console.debug("删除iframe",e.origin, url);
+        //     // e.origin ignore
+        //     [...document.querySelectorAll("iframe")]
+        //         .filter(e => e.src == url)
+        //         .forEach(e => {
+        //             // console.debug("删除了iframe",e.src, e);
+        //             e.remove()
+        //         });
+        // }, false);
+        // document.body.appendChild(新元素(
+        //     '<iframe style="visibility: hidden"></iframe>',
+        //     { src: url + '#下载-' + encodeURI(filename) }))
+        // 或打开新页面
+        var src = url + '#下载-' + encodeURI(filename)
+        window.open(src, "_BLANK")
     }
     var 高清地址を取 = src => src
         .replace('http:', 'https:')
@@ -79,6 +78,12 @@
     }
     var 下载当页 = () => 照片列表を取().forEach(({ url, filename }) => 智能下载(url, filename))
 
+    // UI
+    var 新元素 = (innerHTML, attributes = {}) => {
+        var e = document.createElement("div");
+        e.innerHTML = innerHTML;
+        return Object.assign(e.children[0], attributes)
+    }
     var 生成下载界面 = () => {
         // “更多”按钮前插入一个按钮
         var 下载按钮 = 新元素('<div></div>', {
