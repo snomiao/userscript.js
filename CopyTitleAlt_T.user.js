224abfe5a6fe5ccca54d577a9089cd2984f255da
snomiao <snomiao@gmail.com>
2020-05-29 08:06:54 +0800

    优化取标题方案

--- CopyTitleAlt_T.user.js
+++ CopyTitleAlt_T.user.js
@@ -4,11 +4,12 @@
 // @description     Press Alt+T to copy title and url like this `# ${TITLE}\n${URL}` and Alt+Shift+T to copy the markdown style link `[${TITLE}]( ${URL} )`
 // @description:zh  按 Alt+T 复制 Markdown 格式的链接 `[${TITLE}]( ${URL} )` and Alt+Shift+T 复制 标题和地址 `# ${TITLE}\n${URL}`
 // @namespace       https://userscript.snomiao.com/
-// @version         0.5
+// @version         0.6
 // @author          snomiao@gmail.com
 // @match           *://*/*
 // @grant           none
 // ==/UserScript==
+// (20200425)更新：优化取标题方案
 // (20200425)更新：修改格式
 // (20200423)更新：增加格式
 (function () {
@@ -38,7 +39,9 @@
     };
     var 取标题 = () => {
         var 标题列 = [...document.querySelectorAll('h1')]
-        return 标题列.length == 1 && 标题列[0].innerText.trim() || document.title || ''
+        var 页面标题 = 标题列.length == 1 && 标题列[0].innerText.trim() || ''
+        var 头标题 = document.title || ''
+        return 页面标题.length > 头标题.length ? 页面标题 : 头标题
     }
     window.addEventListener('keydown', (e) => {
         if (e.altKey && !e.shiftKey && !e.ctrlKey && e.code == 'KeyT')
