65dac87a07fa3d14a735f8c602ff1247765ab3d1
snomiao <snomiao@gmail.com>
2021-03-03 19:03:53 +0800

    (20210303)

--- CopyTitleAlt_T.user.js
+++ CopyTitleAlt_T.user.js
@@ -5,11 +5,12 @@
 // @description     Press Alt+T to copy title and url like this `# ${TITLE}\n${URL}` and Alt+Shift+T to copy the markdown style link `[${TITLE}]( ${URL} )`
 // @description:zh  按 Alt+T 复制 Markdown 格式的链接 `[${TITLE}]( ${URL} )` and Alt+Shift+T 复制 标题和地址 `# ${TITLE}\n${URL}`
 // @namespace       https://userscript.snomiao.com/
-// @version         0.7
+// @version         0.7.1
 // @author          snomiao@gmail.com
 // @match           *://*/*
 // @grant           none
 // ==/UserScript==
+// (20210303)更新：优化提示
 // (20200425)更新：优化取标题方案
 // (20200425)更新：修改格式
 // (20200423)更新：增加格式
@@ -32,29 +33,30 @@
             ok = true
         }
         document.body.removeChild(input);
-        ok
-            ? alert(标题地址 + '\n copyied!')
-            : alert('copy title failed, please check browser version')
+        ok || alert('copy title failed, please check browser version')
         return ok || false
     };
     var 取标题 = () => {
         const 最长标题 = [
             document.title,
             ...[...document.querySelectorAll('h1')]
-                .map(e => e.innerText)]
+                .map(e => e.innerText)
+        ]
             .map(str => str.replace(/\r?\n.*/g, ''))
             .sort((a, b) => a.length - b.length)
             .pop()
         return 最长标题
-        // var 标题列 = [...document.querySelectorAll('h1')]
-        // var 页面标题 = 标题列.length == 1 && 标题列[0].innerText.trim() || ''
-        // var 头标题 = document.title || ''
-        // return 页面标题.length > 头标题.length ? 页面标题 : 头标题
     }
     window.addEventListener('keydown', (e) => {
-        if (e.altKey && !e.shiftKey && !e.ctrlKey && e.code == 'KeyT')
+        if (e.altKey && !e.shiftKey && !e.ctrlKey && e.code == 'KeyT'){
             复制标题文本(`[${取标题()}]( ${location.href} )`)
-        if (e.altKey && e.shiftKey && !e.ctrlKey && e.code == 'KeyT')
+            e.preventDefault()
+            e.stopPropagation()
+        }
+        if (e.altKey && e.shiftKey && !e.ctrlKey && e.code == 'KeyT'){
             复制标题文本(`# ${取标题()}\n${location.href}`)
+            e.preventDefault()
+            e.stopPropagation()
+        }
     })
 })();
