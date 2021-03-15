b0de8d9c373548d215c4b4b54e6cd5c2e0b22787
snomiao <snomiao@gmail.com>
2021-03-16 02:29:25 +0800

    fix

--- Google日历自动上色.user.js
+++ Google日历自动上色.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [雪星实验室] Google Calendar 谷歌日历自动上色
 // @namespace    https://userscript.snomiao.com/
-// @version      0.0.5
+// @version      0.0.6
 // @description  【功能测试中, bug反馈：snomiao@gmail.com】Google日历自动上色、根据匹配到的关键词显示特定颜色，例如： 休|睡、洗漱|收拾|整理|日记|日志、研究|学习|探索|背词|了解、上学|上班|上课、健身|锻练|热身、路上|通勤、料理|做饭、仪式|典礼|祭祀、紧急|重要|考试|测验、群聊|交流|玩|游戏|知乎、电影|看书|阅书|影评
 // @author       snomiao@gmail.com
 // @match        *://calendar.google.com/*
@@ -9,26 +9,26 @@
 // ==/UserScript==
 
 // [颜色名 — HTML颜色代码](https://htmlcolorcodes.com/zh/yanse-ming/)
-var 颜色表 = {
+var 正则颜色表 = {
     '休|睡': 'Azure',
-    '洗漱|收拾|购物|购买|整理|日记|日程|日志|维护': "LightYellow",
-    '科研|研发|开发|研究|探索': "deepskyblue",
+    '洗漱|收拾|购物|购买|整理|日记|日程|日志|维护|运维|配置': "LightYellow",
     '上学|上班|上课|课程': "skyblue",
     '健身|锻练|热身': "Lime",
     '路上|通勤': "Gainsboro",
-    '料理|做饭': "lawngreen",
+    '料理|做饭|吃': "lawngreen",
     '仪式|典礼|祭祀': 'yellow',
-    '紧急|重要|考试|测验': 'OrangeRed',
+    '紧急|重要|考试|测验|吵架|事故': 'OrangeRed',
     '群聊|交流|玩|游戏|知乎|微博|小红书|视频|抖音|bilibili|B站': "Violet",
+    '思考|科研|研发|开发|研究|探索': "deepskyblue",
     '学习|背词|了解|阅读|电影|看书|阅书|影评|《': "Turquoise",
 }
 var 更新颜色 = () => {
     var 事件元素列 = [...document.querySelectorAll('div[data-eventid]')]
     var 颜色分析 = 事件元素列.map(e => ({ 元素: e, 文本: e.textContent, 颜色: window.getComputedStyle(e).getPropertyValue('background-color') }))
-    颜色分析.forEach(({ 元素, 文本 }) => Object.keys(颜色表)
+    颜色分析.forEach(({ 元素, 文本 }) => Object.keys(正则颜色表)
         .filter(正则 => 文本.match(正则))
         .sort((正则1, 正则2) => 文本.match(正则1).index - 文本.match(正则2).index)
-        .map(正则 => 元素.style.backgroundColor = 颜色表[正则]))
+        .map(正则 => 元素.style.backgroundColor = 正则颜色表[正则]))
 }
 
 var 节流 = (间隔, 函数 = async () => null, 提示函数 = async () => null, 上次执行 = 0) => async (...参数) =>
