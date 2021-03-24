e22e001e8753419f476ed25ba9f6c8b1cd4ff60f
snomiao <snomiao@gmail.com>
2021-03-24 16:15:59 +0800

    fix: 代码不应设定 @name:en，因为「en」是默认的语系。, 代码不应设定 @description:en，因为「en」是默认的语系。

--- LinksList2toN.user.js
+++ LinksList2toN.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name            LinksList2toN
 // @name:zh         Alt + 123... 一键批量打开谷歌必应搜索的前2的n次方项搜索结果
-// @name:en         Alt + 123... One-click batch open Google Bing search first 2 n-th power search results
 // @namespace       snomiao@gmail.com
 // @version         1.0.0
-// @description:en  To quickly understand a field, press Alt+1 ...2,3,4...Alt+5 on the search page of Google or Bing to open the search results of the first 2 nth power items and copy the opened ones link. Currently supports: Google, Bing, Zhihu.
+// @description  To quickly understand a field, press Alt+1 ...2,3,4...Alt+5 on the search page of Google or Bing to open the search results of the first 2 nth power items and copy the opened ones link. Currently supports: Google, Bing, Zhihu.
 // @description:zh  快速了解一个领域用，在谷歌或必应的搜索页面 按 Alt+1 ...2,3,4... Alt+5  将会打开前2的n次方项的搜索结果，并复制打开的链接。目前支持：谷歌、必应、知乎。
 // @author          snomiao
 // @match           *
