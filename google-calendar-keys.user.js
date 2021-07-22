1c2e4882cd53d0f58be0a5e1d3c5313872ce77c0
snomiao <snomiao@gmail.com>
2021-07-22 08:29:28 +0800

    fix: 更新版本号

--- google-calendar-keys.user.js
+++ google-calendar-keys.user.js
@@ -3,7 +3,7 @@
 // @name:zh      [雪星实验室] Google Calendar with Keyboard Enhanced
 // @namespace    https://userscript.snomiao.com/
 // @version      0.0.2
-// @description  【功能测试中, bug反馈：snomiao@gmail.com】Google日历键盘增强，雪星自用，功能：复制日程视图里的文本内容
+// @description  【功能测试中, bug反馈：snomiao@gmail.com】Google日历键盘增强，雪星自用，功能：双击复制日程视图里的文本内容
 // @author       snomiao@gmail.com
 // @match        *://calendar.google.com/*
 // @grant        none
