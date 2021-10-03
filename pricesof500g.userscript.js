8449f759de74af6a0a29c935f969fde8fa8e731c
Snowstar Cyan <snomiao@gmail.com>
2021-10-04 04:11:53 +0800

    fix: update version number

--- pricesof500g.userscript.js
+++ pricesof500g.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         淘宝、京东、天猫自动按每斤价格排序 TAOBAO/JD/TMALL / Automatic sort by 500g price.
 // @namespace    snomiao@gmail.com
-// @version      1.1.4
+// @version      1.1.5
 // @description  已知bug：淘宝的价格和商品标题上写的重量通常对不上，此bug无法修复，天猫、京东暂无此问题, 标题出现2个以上重量单位的按最后一个算 ( bug反馈联系： snomiao@gmail.com 或 qq 997596439 )
 // @author       snomiao@gmail.com
 // @match        http*://cart.jd.com/cart*
