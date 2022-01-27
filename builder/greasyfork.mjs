e79c1e112b93cf61be622029d46aee55afeeb3cc
snomiao <snomiao@gmail.com>
2022-01-27 22:42:44 +0800

    chore: greasyfork

--- builder/greasyfork.mjs
+++ builder/greasyfork.mjs
@@ -1,25 +1,31 @@
 import fs from 'fs/promises';
 import userscriptMeta from 'userscript-meta';
 import { minify } from 'terser';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 const pmap = (ls, fn) => Promise.all(ls.map(fn));
-await pmap(
+
+const scriptparser = async (filename) => {
+    const content = await fs.readFile('./src/' + filename, 'utf8');
+    const header =
+        content
+            .trim()
+            .match(/^(?:^\/\/.*\s?)+/gm)
+            ?.join('\n') || '';
+    const meta = userscriptMeta.parse(header);
+    // const header2 = userscriptMeta.stringify(meta);
+    const _url = `https://raw.githubusercontent.com/snomiao/userscript.js/master/src/${filename}`;
+    const url = encodeURI(_url);
+    // console.log('\nminifying ' + filename);
+    const mincode = (await minify(content, { compress: true })).code;
+    // console.log(filename, meta, url);
+    return { filename, content, meta, url, mincode };
+};
+
+const sinfo = await pmap(
     (await fs.readdir('./src/')).filter((e) => e.match(/\.user\.js$/)),
-    async (filename) => {
-        const content = await fs.readFile('./src/' + filename, 'utf8');
-        const header =
-            content
-                .trim()
-                .match(/^(?:^\/\/.*\s?)+/gm)
-                ?.join('\n') || '';
-        const meta = userscriptMeta.parse(header);
-        // const header2 = userscriptMeta.stringify(meta);
-        const _url = `https://raw.githubusercontent.com/snomiao/userscript.js/master/src/${filename}`;
-        const url = encodeURI(_url);
-        // console.log('\nminifying ' + filename);
-        const mincode = (await minify(content, { compress: true })).code;
-        console.log(url)
-        // console.log(filename, meta, url);
-        return { filename, content, meta, url, mincode };
-    }
+    scriptparser
 );
 
+sinfo.map((e) => console.log(e.url));
+await promisify(exec)('https://greasyfork.org/en/import');
--- src/batch-search.user.js
+++ src/batch-search.user.js
@@ -9,21 +9,28 @@
 // @grant        none
 // ==/UserScript==
 
-const engs = {
+const engines = {
     Page: {
         Google: 'https://www.google.com/search?q=%s',
         LuckyGoogle: 'https://www.google.com/search?btnI&q=%*',
+        Magi: 'https://magi.com/search?q=%s',
     },
-    Knowledge: {},
+    Knowledge: {
+        Magi: 'https://magi.com/search?q=%s',
+        Google: 'https://www.google.com/search?q=%s',
+        Wiki: 'https://www.google.com/search?q=%s',
+        FreeMDict: 'https://forum.freemdict.com/search?q=%s',
+        FreeMDict: 'https://forum.freemdict.com/search?q=%s',
+    },
     Book: {
-        douban: 'https://search.douban.com/book/subject_search?search_text=%*',
         Book4you: 'https://book4you.org/s/%*',
+        douban: 'https://search.douban.com/book/subject_search?search_text=%*',
     },
     Torrent: { 1337: 'https://www.1377x.to/search/%*/1/' },
     Movie: {},
     Video: {},
     Music: {},
-    Dict: { FreeMDict: 'https://forum.freemdict.com/search?q=%s' },
     Model: { Google: 'https://www.google.com/search?q=%s' },
     Image: {},
 };
+const enginess = JSON.parse(JSON.stringify(engines).replace(/%s|%\*/g, s));
