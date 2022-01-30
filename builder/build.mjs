5b35f2a65faf9ccba26e36cbbedb6733b51d5796
snomiao <snomiao@gmail.com>
2022-01-30 21:08:25 +0800

    chore: scripts description

--- builder/build.mjs
+++ builder/build.mjs
@@ -20,23 +20,33 @@ const aStyle1 =
     'display:inline-block;background:#32eb00;padding:0.1rem 0.5rem;border-radius:0.2rem;text-decoration:none;color:black;';
 const aStyle2 =
     'display:inline-block;background:#32a0eb;padding:0.1rem 0.5rem;border-radius:0.2rem;text-decoration:none;color:white;';
-const ss = sinfo
-    .map(({ meta, filename, mincode, url, bookmarklet }) => {
-        const name = meta.name;
-        const description = meta.description;
-        console.log(meta);
-        const md = `[${filename}](javascript:${encodeURIComponent(mincode)})`;
-        const src = jsDelivrPath + filename;
-        const h3 = `<h3>${name}</h3>`;
-        const gfurlRaw = `https://greasyfork.org/en/scripts?filter_locale=0&q=${name}`;
-        const gfurl = encodeURI(gfurlRaw);
-        const a1 = `<p>GreasyFork安装：<a style="${aStyle1}" href="${gfurl}" title="${description}">GreasyFork</a></p>`;
-        const a2 = `<p>拖到收藏夹安装：<a style="${aStyle2}" href="${bookmarklet}" title="${description}">${name}</a></p>`;
-        const desc = `<p>描述：${description}</p>`;
-        const element = [h3, a1, a2, desc, ''].join('\n');
-        return element;
-    })
-    .join('\n');
+const ss =
+    '# 脚本列表\n' +
+    `## 安装方法\n` +
+    `1. TamperMonkey User - 点击 <a style="${aStyle1}">GreasyFork</a> 按钮，进入 GreasyFork 脚本安装页面安装。 \n` +
+    `2. Bookmarklet - 把对应的 <a style="${aStyle2}">脚本名称</a> 按钮，拖到 收藏夹/书签栏 内安装，在需要的时候点击使用（或搜索对应书签名称使用）。 \n` +
+    `\n` +
+    '## 脚本列表\n\n' +
+    sinfo
+        .map(({ meta, filename, mincode, url, bookmarklet }) => {
+            const name = meta.name;
+            const description = meta.description;
+            console.log(meta);
+            const md = `[${filename}](javascript:${encodeURIComponent(
+                mincode
+            )})`;
+            const src = jsDelivrPath + filename;
+            const h3 = `### ${name}`;
+            const gfurlRaw = `https://greasyfork.org/en/scripts?filter_locale=0&q=${name}`;
+            const gfurl = encodeURI(gfurlRaw);
+            const a1 = `<p>GreasyFork安装：<a style="${aStyle1}" href="${gfurl}" title="${description}">GreasyFork</a></p>`;
+            const a2 = `<p><a style="${aStyle2}" href="${bookmarklet}" title="${description}">${name}</a></p>`;
+            const desc = `<p>描述：${description}</p>`;
+            const element = [h3, a1, a2, desc, ''].join('\n');
+            return element;
+        })
+        .join('\n') +
+    '\n---';
 await fs.writeFile('./docs/script-intro.md', ss);
 
 // await promisify(exec)('chrome https://greasyfork.org/en/import');
