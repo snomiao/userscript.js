3336236bde156d29270616c5bef1e84f19aca238
snomiao <snomiao@gmail.com>
2022-02-18 11:54:53 +0800

    chore(subtree): pre-push sync subtree

--- subtree.bat
+++ subtree.bat
@@ -1,4 +1,4 @@
 
-# sync snoread
+@REM # sync snoread
 git subtree pull -P extensions/snoread git@github.com:snolab/snoread master --squash
 git subtree push -P extensions/snoread git@github.com:snolab/snoread master
