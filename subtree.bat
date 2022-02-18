e9e3aaa5f84e80a60b5d4c359e1da2a759c36963
snomiao <snomiao@gmail.com>
2022-02-18 11:43:35 +0800

    chore: snoread subtree for snolab

--- subtree.bat
+++ subtree.bat
@@ -1,4 +1,4 @@
 
 # sync snoread
-git subtree pull -P extensions/snoread git@github.com:snomiao/snoread master --squash
-git subtree push -P extensions/snoread git@github.com:snomiao/snoread master
+git subtree pull -P extensions/snoread git@github.com:snolab/snoread master --squash
+git subtree push -P extensions/snoread git@github.com:snolab/snoread master
