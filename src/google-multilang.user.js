0d0f316d9f568fc6231a9674a1151bac2db595f4
snomiao <snomiao@gmail.com>
2022-06-26 19:42:50 +0800

    fix(mulango): desc

--- src/google-multilang.user.js
+++ src/google-multilang.user.js
@@ -3,9 +3,9 @@
 // @name:zh            谷歌多语言搜索 en/zh
 // @namespace          snomiao@gmail.com
 // @author             snomiao@gmail.com
-// @version            0.0.4
-// @description        View a google search result in two languages side by side for comparison and language learning. now supports Bing & Google
-// @description:zh     以并列多语言视角浏览谷歌搜索结果 现支持 Bing & Google
+// @version            0.0.5
+// @description        [snolab] Mulango - Walkers for bilingual learners. View a google search result in two languages side by side for comparison and language learning. now supports Bing & Google,
+// @description:zh     [snolab] Mulango - 双语学习者的学步车，以并列多语言视角浏览谷歌搜索结果 现支持 Bing & Google,
 // @match              https://*.google.com/search?*
 // @match              https://*.bing.com/search?*
 // @match              https://*/search*
