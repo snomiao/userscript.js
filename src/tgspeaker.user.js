85dd2e02ee06ae4248ee6a833037d54cbd9deee3
snomiao <snomiao@gmail.com>
2022-08-16 15:34:05 +0800

    fix(tgspeaker): use aiife

--- src/tgspeaker.user.js
+++ src/tgspeaker.user.js
@@ -3,7 +3,7 @@
 // @namespace          snomiao@gmail.com
 // @author             snomiao@gmail.com
 // @version            0.1.0
-// @description        Speak latest telegram message With TTS technology.
+// @description        Speak latest telegram message With TTS technology just in your browser.
 // @match              https://*.telegram.org/z/
 // @grant              none
 // @run-at             document-start
@@ -21,24 +21,30 @@ piserve | snosay --voice "Microsoft Huihui Desktop"
 */
 
 
-const say = async s=>{
-    if(!s) return;// console.error('say empty msg')
+async function say(s) {
+    if (!s)
+        return; // console.error('say empty msg')
+
     // new method to say
-    console.log('saying '+s);
-    if(globalThis.speechSynthesis) {
+    console.log('saying ' + s);
+    if (globalThis.speechSynthesis) {
         // wait for voices
-        while(speechSynthesis.getVoices().length === 0){
-            await new Promise(r=>setTimeout(r, 1e3))
+        while (speechSynthesis.getVoices().length === 0) {
+            await new Promise(r => setTimeout(r, 1e3));
         }
         const utter = new SpeechSynthesisUtterance(s);
-        utter.voice =speechSynthesis.getVoices().filter(({lang})=>navigator.languages.includes(lang)).reverse()[0],
-        utter.rate = Math.min(Math.max(1, s.length / 60), 4)
-        speechSynthesis.speak(utter)
+        utter.voice = speechSynthesis.getVoices().filter(({ lang }) => navigator.languages.includes(lang)).reverse()[0],
+            utter.rate = Math.min(Math.max(1, s.length / 60), 4);
+        speechSynthesis.speak(utter);
     }
-    else fetch('http://localhost:25971/?text='+encodeURIComponent(s))
-}
+    else
+        fetch('http://localhost:25971/?text=' + encodeURIComponent(s));
+};
 const lastMsg = ()=>[...document.querySelectorAll('.Message:not(.own) .text-content')].map(e=>e.textContent).reverse()[0]
 const chagnedFilterMaker = (init)=>(e )=> e !== init? (init =e): undefined
-const changedFilter = chagnedFilterMaker('')
+const changedFilter = chagnedFilterMaker('')j j
 const looper = ()=>(say(changedFilter(lastMsg())), 1)
-while(looper()) await new Promise(r=>setTimeout(r,1000))
+
+(async function() {
+    while(looper()) await new Promise(r=>setTimeout(r,1000))
+})();
