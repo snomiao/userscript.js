61c4fe2c9f07e795420cb70675e81fde5d17a69d
snomiao <snomiao@gmail.com>
2023-01-22 20:35:38 +0900

    fix(builder): buildts

--- builder/buildts.ts
+++ builder/buildts.ts
@@ -1,47 +1,61 @@
-import { globby } from 'globby';
-import esbuild from 'esbuild';
 import chalk from 'chalk';
+import esMain from 'es-main';
+import esbuild from 'esbuild';
+import { watch, writeFile } from 'fs/promises';
+import { globby } from 'globby';
 import readFileUtf8 from 'read-file-utf8';
-import { Writer } from 'steno';
+{
+    if (esMain(import.meta)) main();
+}
+export async function main() {
+    await buildTs();
+}
 
-const ls = await globby('ts/*.user.ts');
-
-// return;
-console.log(ls);
-
-console.log(
-    await Promise.all(
-        ls.map(async (f) => {
-            await userscriptBuild(f);
-            return;
+async function buildTs() {
+    const tsInputs = await globby('ts/*.user.ts');
+    console.log({ ls: tsInputs });
+    const jsOutputs = await Promise.all(
+        tsInputs.map(async (entry) => {
+            await userscriptBuild(entry).catch(console.error);
+            for await (const _event of watch(entry))
+                await userscriptBuild(entry).catch(console.error);
         })
-    )
-);
-async function userscriptBuild(f: string) {
-    const content = await readFileUtf8(f);
-    const rawHead = content.match(
-        /\/\/\s=+Userscript=+\n[\s\S]*?[^\\]\n\n/i
-    )?.[0];
-    if (!rawHead) throw new Error('Userscript head not found in ' + f);
+    );
+    console.log(jsOutputs);
+}
 
-    const head = rawHead;
-    console.log('head', chalk.green(head));
+async function userscriptBuild(tsEntry: string) {
+    const content = await readFileUtf8(tsEntry);
+    if (!content) return;
+    const header = content.replace(/\r/g, '').match(/^(\/\/.*\n)+/i)?.[0];
+    if (!header) throw new Error('Userscript header not found in ' + tsEntry);
+
+    const banner = `${header}\n/* eslint-disable */`;
+    console.log('banner', chalk.green(banner));
 
     console.log('building contents');
     const buildResult = await esbuild.build({
         outdir: 'src',
-        entryPoints: [f],
-        legalComments: 'none',
+        entryPoints: [tsEntry],
+        legalComments: 'eof',
         platform: 'browser', //will convert into iife
+        format: 'esm',
         target: 'esnext',
         bundle: true,
-        banner: { js: head },
+        banner: { js: banner },
         write: false,
     });
-    const { path, text } = buildResult.outputFiles[0];
-    // const outContent = head + '\n' + text;
-    const outContent = text;
-    console.log('writing', chalk.cyan(outContent));
-    await new Writer(path).write(outContent);
-    console.log(chalk.greenBright('ALL DONE'));
+    const outfile = buildResult.outputFiles[0];
+    if (!outfile) throw new Error('No output file');
+    const { path: jsPath, text } = outfile;
+    const outContent = text
+        .replace(/(?<=^\s*\/\/.*)node_modules(?:.*?node_modules)\//gm, '')
+        .replace(/(?<=^\s*\/\/.*)\/dist\/.*/gm, '')
+        .replace(/(?<=^\s*\/\/.*)ts\//gm, '')
+        .replace(/(?<=^\s*\/\/.*)(\.user)?\.(?:[cm]?[jt]s)/gm, '');
+    // console.log('writing', chalk.cyan(outContent));
+
+    await writeFile(jsPath, outContent);
+    console.log(chalk.greenBright(`Built ${jsPath}`));
+    return jsPath;
 }
