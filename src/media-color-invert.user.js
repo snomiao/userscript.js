87cdaa85532107c3f9529d8e3563c4b963fe36d9
snomiao <snomiao@gmail.com>
2023-03-10 19:47:06 +0900

    fix(src): color inverter
    color inverter

--- src/media-color-invert.user.js
+++ src/media-color-invert.user.js
@@ -2,14 +2,24 @@
 // @name             [SNOLAB] media-color-invert with alt+i
 // @namespace        https://userscript.snomiao.com/
 // @version          1.1.0
-// @description      Revert color by alt+i
+// @description      Invert color by alt+i
 // @author           snomiao@gmail.com
 // @match            *://*/*
-// @grant            none
 // @contributionURL  https://snomiao.com/donate
 // @supportURL       https://github.com/snomiao/userscript/issues
+// @grant       GM.getValue
+// @grant       GM_getValue
+// @grant       GM.setValue
+// @grant       GM_setValue
 // ==/UserScript==
 
+const state = {
+  invert: (await globalThis.GM?.getValue("media-color-invert")) ?? true,
+};
+
+const observer = new MutationObserver(function (mutations) {
+  scan();
+});
 window.addEventListener(
   "keydown",
   (e) =>
@@ -18,7 +28,17 @@ window.addEventListener(
     !e.shiftKey &&
     !e.ctrlKey &&
     e.code === "KeyI" &&
-    [...document.querySelectorAll("video,img")].map(
-      (e) => (e.style.filter = "invert(1) hue-rotate(0)")
-    )
+    toggle()
 );
+scan();
+
+async function toggle() {
+  state.invert = !state.invert;
+  scan();
+  await globalThis.GM?.setValue("media-color-invert", state.invert);
+}
+function scan() {
+  [...document.querySelectorAll("video,img")].map(
+    (e) => (e.style.filter = state.invert ? "invert(1) hue-rotate(0)" : "")
+  );
+}
