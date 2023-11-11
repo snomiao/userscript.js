bdf7524a779b95a2bc036ef5de6e599f26f8ed01
snomiao <snomiao@gmail.com>
2023-11-11 22:37:03 +0000

    fix(src): flag
    flag

--- src/npm-typescript-flag.user.js
+++ src/npm-typescript-flag.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name             [SNOLAB] NPM Typescript Flag
 // @namespace        https://userscript.snomiao.com/
-// @version          0.1.0
+// @version          0.1.1
 // @description      Show "Dt" and "Ts" Icon on npm package search result.
 // @author           snomiao@gmail.com
 // @match            *://www.npmjs.com/search*
@@ -11,31 +11,49 @@
 // @supportURL       https://github.com/snomiao/userscript.js/issues
 // ==/UserScript==
 
+console.log("[SNOLAB] NPM Typescript Flag v0.1.5");
+let lastFetch = 0;
+let fails = 0;
+
 const observer = new IntersectionObserver((entries) =>
   entries.forEach(async (entry) => {
     if (!entry.isIntersecting) return;
-    const h3 = entry.target;
-    if (h3.ariaChecked) return;
-    //const rect = h3.getBoundingClientRect();
-    //const isVisible = 0 < rect.bottom  && rect.top < window.innerHeight;
-    // if(!isVisible) return;
-    const pathname = h3.parentElement.pathname;
-    const content = await (
-      await fetch(pathname, { cache: "force-cache" })
-    ).text();
-    const hasDt =
-      content.match(
-        '(?<="true">)<img.*?This package has TypeScript declarations provided by.*?/>'
-      )?.[0] ?? "";
-    const hasTs =
-      content.match(
-        '(?<="true">)<img.*?This package contains built-in TypeScript declarations.*?/>'
-      )?.[0] ?? "";
-    h3.innerHTML = h3.innerText + " / " + hasDt + hasTs;
-    console.log({ pathname, hasDt, hasTs });
-    h3.ariaChecked = true;
+    while (!(await markPackage(entry.target)));
   })
 );
+
+async function markPackage(h3) {
+  if (h3.ariaChecked) return true;
+  // back-off retry
+  const interval = 200;
+  const wait = interval * 2 ** fails;
+  const until = lastFetch + wait;
+  while (+new Date() < until)
+    await new Promise((r) => setTimeout(r, (until - +new Date()) / 2));
+  lastFetch = +new Date();
+  //
+  const pathname = h3.parentElement.pathname;
+  const response = await fetch(pathname, { cache: "force-cache" });
+  if (response.status !== 200) {
+    fails++;
+    return false; // will try again later
+  }
+  fails /= 2; // find a balanced request rate
+
+  const content = await response.text();
+  const hasDt =
+    content.match(
+      '(?<="true">)<img.*?This package has TypeScript declarations provided by.*?/>'
+    )?.[0] ?? "";
+  const hasTs =
+    content.match(
+      '(?<="true">)<img.*?This package contains built-in TypeScript declarations.*?/>'
+    )?.[0] ?? "";
+  h3.innerHTML = h3.innerText + " / " + hasDt + hasTs;
+  console.log({ pathname, hasDt, hasTs });
+  h3.ariaChecked = true;
+  return true;
+}
 
 function $$(sel, el = document) {
   return [...el.querySelectorAll(sel)];
