5a0f0eb179128321243ee43e4b6470adca5de961
snomiao <snomiao@gmail.com>
2023-11-16 13:10:21 +0000

    fix(builder): stage
    stage

--- builder/greasyfork.ts
+++ builder/greasyfork.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import clipboard from 'clipboardy';
+// import clipboard from 'clipboardy';
 import { globby } from 'globby';
 import { promisify } from 'util';
 
@@ -8,17 +8,17 @@ const scripts = await globby('**/*.user.js');
 const out = await Promise.all(
     scripts.map((path) => {
         return encodeURI(
-            'https://raw.githubusercontent.com/snomiao/userscript.js/master/' +
+            'https://raw.githubusercontent.com/snomiao/userscript.js/main/' +
                 path
         );
     })
 );
 const outString = out.join('\n');
 console.log(outString);
-await clipboard.write(outString);
+// await clipboard.write(outString);
 
 console.log(
     'userscript links copied, please open import page to paste https://greasyfork.org/en/import'
 );
 
-await promisify(exec)('cmd /c start chrome https://greasyfork.org/en/import');
+// await promisify(exec)('cmd /c start chrome https://greasyfork.org/en/import');
